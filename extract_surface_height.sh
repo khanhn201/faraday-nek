41a667823b4de41c7d1c19ae8c76f103a03ddd2b
Khanh Nguyen <80285371+khanhn201@users.noreply.github.com>
2025-09-29 23:35:40 -0500

    Update extract_surface_height.sh

--- extract_surface_height.sh
+++ extract_surface_height.sh
@@ -1,1 +1,1 @@
-grep -e "Surface height" -e "Step" logfile  | head -10 | awk  '/Step/ {t=$4} /Surface height/ {print t, $3}'   > surface_height.dat
+grep -e "Surface height" -e "Step" logfile | awk  '/Step/ {t=$4} /Surface height/ {print t, $3}'   > surface_height.dat
