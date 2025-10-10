15d0dceafbbf1928c7d83710eeffc53670ec4040
Sitanan Tainpakdipat <sitanan2@illinois.edu>
2025-10-10 09:03:53 -0500

    feat: add data comparison plot to Fig9

--- plot_height.m
+++ plot_height.m
@@ -1,18 +1,28 @@
-omega = 10.5718
-mag = 0.04474
-l = 0.05
-v = omega*mag
+clear;close all; clc
+omega = 10.5718;
+mag = 0.04474;
+l = 0.05;
+v = omega*mag;
 
 data = dlmread('surface_height.dat', ',');
 
+% t = data(:,1)*omega/(2*pi);
 t = data(:,1)*(l/v)*omega/(2*pi); % Paper plot t*omega/(2pi)
 h = data(:,2);
 
 % Plot
-figure;
-plot(t, h, 'LineWidth', 1.5);
-xlabel('t');
-ylabel('h');
+fig = figure;
+% plot(t+97.5, h, 'LineWidth', 1.5, 'DisplayName','Present work');
+plot(t+81.6, h, 'LineWidth', 1.5, 'DisplayName','Present work');
+xlabel('t');ylabel('h');
 title('Surface Height vs Time');
 grid on;
 
+hold on
+% Reference data
+dataref = load('plot_fig9.csv');
+plot(dataref(:,1),dataref(:,2)+0.98,'--','LineWidth',1.5, 'DisplayName','Reference Fig.9')
+xlim([100,125]);
+legend('Location','best');
+
+exportgraphics(fig,'plot_height.png','ContentType','vector')
